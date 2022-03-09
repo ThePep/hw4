9f8c29a9f4d69586199f032333dce71cacaadad7
ThePep <jesse.laux@kellogg.northwestern.edu>
2022-03-09 19:02:13 +0000

    draft 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({email:params["email"]})
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session[:user_id] = @user.id
+        flash[:notice] = "Welcome, #{@user.username}."
+        redirect_to "/"
+      else 
+        flash[:notice] = "Incorrect password entered."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Email does not exist."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to "/sessions/new"
   end
+
 end
   
