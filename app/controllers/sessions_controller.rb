f6742af4b800e9803027ad25269b0a1cc4a4150a
ThePep <jesse.laux@kellogg.northwestern.edu>
2022-03-09 20:16:05 +0000

    draft 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by({email:params["email"]})
-    if @user
+   
       if BCrypt::Password.new(@user.password) == params["password"]
         session[:user_id] = @user.id
         flash[:notice] = "Welcome, #{@user.username}."
-        redirect_to "/"
+        redirect_to "/places"
       else 
         flash[:notice] = "Incorrect password entered."
         redirect_to "/sessions/new"
